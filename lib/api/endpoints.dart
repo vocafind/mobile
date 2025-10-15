eba26f9752ae4179ea718f59530067176783e099
Muhammad Nasyith Aditya Putera <mnasyith2006@gmail.com>
2025-10-15 23:50:01 +0700

    local changes

--- lib/api/endpoints.dart
+++ lib/api/endpoints.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = "http://10.136.139.44:5035/api";  //ganti 10.136.149.51 dengan ip address komputer 
+  static const String baseUrl = "http://10.170.13.244:5035/api";  //ganti 10.136.149.51 dengan ip address komputer 
 
 
   // -------------------------------------------------------------TALENTS
