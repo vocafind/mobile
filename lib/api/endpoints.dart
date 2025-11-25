47701909f5fbf5d9845c0a0b789efb44c0e86a57
Muhammad Nasyith Aditya Putera <mnasyith2006@gmail.com>
2025-11-25 19:04:44 +0700

    Job fair detail

--- lib/api/endpoints.dart
+++ lib/api/endpoints.dart
@@ -1,7 +1,7 @@
 class ApiConfig {
   // Ganti IP dengan IP komputer server kamu
-  static const String baseUrl = "http://10.136.145.172:5035/api";
-  static const String baseUrlFoto = "http://10.136.145.172:5035/";
+  static const String baseUrl = "http://10.136.153.37:5035/api";
+  static const String baseUrlFoto = "http://10.136.153.37:5035/";
   // static const String baseUrl = "https://trpl306-001-site1.ntempurl.com/api";
   // static const String baseUrlFoto = "https://trpl306-001-site1.ntempurl.com/";
 
